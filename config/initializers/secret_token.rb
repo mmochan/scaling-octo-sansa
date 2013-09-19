764845c2c306902f35771c77a4fc154ab4a4a116
Mike Mochan <mmochan@mac.com>
2013-09-19 19:06:35 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spinner::Application.config.secret_key_base = '2534c8a34bcfacf0c5446cff1c8f860902cfabd76ed0f07399a63467541d09e2f04ea6071ad620481a26b80592e46d6504775f23888215273d9d5975936a7e3f'
